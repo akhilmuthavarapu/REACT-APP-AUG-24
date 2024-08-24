6bc924bb3ebc657f31823e487f139afeada9b72a
akhilmuthavarapu21kh1a6132 <21kh1a6132@gmail.com>
2024-08-24 21:51:23 +0530

    FIRST APP

---
+++ table-data-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Listdata from './components/Listdata'
+import Tabledata from './components/tabledata'
+
+export default function App() {
+  return (
+    <div>APP
+      <Tabledata/>
+      <Listdata/>
+
+
+
+      
+
+    </div>
+  )
+}
---
+++ table-data-app/src/components/Listdata.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Listdata() {
+  return (
+    <div>
+       <ul class="list-group">
+  <li class="list-group-item">AKHIL</li>
+  <li class="list-group-item">JOHNY THE CB</li>
+  <li class="list-group-item">CHAKRI THE DON</li>
+  <li class="list-group-item">NAVEEN THE LB</li>
+  <li class="list-group-item">SAI THE MAGAJATHI ANIMUTHYAM</li>
+  <li class="list-group-item">SRAVAN THE BABU SIR BABU ANTHE</li>
+</ul>
+    </div>
+  )
+}
